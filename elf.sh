ad2ffdadfc852b01491cdc4f9a2eec3a04ea8e00
BHanbashi <bhanbashi@gmail.com>
2020-08-30 12:34:21 +0300

    toleration added to elasticsearch

--- elf.sh
+++ elf.sh
@@ -4,7 +4,7 @@ kubectl apply -f elf.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf
+helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf --tolerations=[key:'key', operator:'Exists', effect: 'NoExecute']
 helm install fluent-bit fluent/fluent-bit --namespace=elf
 helm install kibana elastic/kibana --version=7.9.0 --namespace=elf --set service.type=LoadBalancer
 kubectl run random-logger --image=chentex/random-logger -n elf
